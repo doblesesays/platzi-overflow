4a79271fcfa94bde53f88bf33af8b34723ca0b08
Genessis Jimenez <GJimenez@pt.local>
2018-01-27 18:49:58 -0400

    estdo actual

--- src/app/question/question-form.component.ts
+++ src/app/question/question-form.component.ts
@@ -1,8 +1,18 @@
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 import { Component } from '@angular/core';
+import { Question } from './question.model';
 
 @Component ({
     selector: 'app-question-form',
     templateUrl: './question-form.component.html'
 })
 
-export class QuestionFormComponent {};
+export class QuestionFormComponent {
+    onSubmit (form: NgForm) {
+        const q = new Question(
+            form.value.title,
+            form.value.description
+        );
+        console.log(q);
+    }
+};
