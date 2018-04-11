6d5e057e02b7444068f4143fb79e3692cab2c233
GENESSISJ\Usuario <genesis.jz.93@gmail.com>
2018-04-11 10:44:24 -0400

    ruta de singin en el backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,3 +4,45 @@ import Debug from 'debug';
 const app = express.Router()
 const debug = new Debug('platzi-overflow:auth')
 
+const users = {
+    firstName: 'Genessis',
+    lastName: 'Jimenez',
+    email: 'genesis.jz.93@gmail.com',
+    password: '123456',
+    _id: 123
+}
+
+const findUserByEmail = e => users.find(({ email }) => email === e)
+const comparePassword = (providedPassword, userPassword) => {
+    return providedPassword === userPassword
+}
+
+// ESTO ES EQUIVALENTE A LA SENTENCIA DE ARRIBA
+// function findUserByEmail(email) {
+//     return users.find(user=> user.email === email)
+// }
+
+app.post('/signin', (res, req, next) => {
+    const {email, password} = req.body
+    const user = findUserByEmail(email)
+
+    if (!user) {
+        debug('user with email ${email} not found')
+        return handleLoginFailed(res)
+    }
+
+    if (!comparePassword(password, user.password)) {
+        debug('Password dont match ${password} !== ${user.password}')
+        return handleLoginFailed(res)
+    }
+
+})
+
+function handleLoginFailed(res) {
+    return res.status(401).json({
+        message: 'Login faild',
+        error: 'email or password dont match'
+    })
+}
+
+export default app
