9f95c82caf790201f9ff5516be2e2c65213b84b6
doblesesays <genesis.jz.93@gmail.com>
2019-01-16 17:00:03 -0400

    Conectando el signup con la base de datos

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,18 +4,6 @@ import jwt from 'jsonwebtoken';
 
 const debug = new Debug('platzi-overflow:auth-middleware')
 
-export const users = [{
-    firstName: 'Genessis',
-    lastName: 'Jimenez',
-    email: 'genesis.jz.93@gmail.com',
-    password: '0000',
-    _id: 123
-}]
-
- export function findUserByEmail(email) {
-     return users.find(user=> user.email === email)
- }
-
 export const required = (req, res, next) => {
     jwt.verify(req.query.token, secret, (err, token) => {
         if (err) {
