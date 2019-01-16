1b50f8cd94aae3a31d56654505a0016d3b657faa
doblesesays <genesis.jz.93@gmail.com>
2019-01-16 16:35:50 -0400

    Obteniendo una pregunta de la base de datos

--- server/db-api/question.js
+++ server/db-api/question.js
@@ -7,5 +7,19 @@ export default {
     findAll: async () => {
         debug('find all questions')
         return await Question.find().populate('answers')
+    },
+
+    findById: async (_id) => {
+        debug('find one question with id: ', id)
+        return await Question.findOne({ _id })
+            .populate('user')
+            .populate({
+                path: 'answers',
+                options: { sort: '-createdAt' },
+                populate: {
+                    path: 'user',
+                    model: 'User',
+                }
+            })
     }
 }
